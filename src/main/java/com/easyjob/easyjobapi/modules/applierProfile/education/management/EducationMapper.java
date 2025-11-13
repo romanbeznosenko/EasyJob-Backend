f85c75fe74881b28c743991d4fa34dad3388f796
Roman Beznosenko <r.beznosenko@smartsoftsolutions.pl>
2025-11-13 08:34:37 +0100

    Added education mapper to response

--- src/main/java/com/easyjob/easyjobapi/modules/applierProfile/education/management/EducationMapper.java
+++ src/main/java/com/easyjob/easyjobapi/modules/applierProfile/education/management/EducationMapper.java
@@ -3,6 +3,7 @@ package com.easyjob.easyjobapi.modules.applierProfile.education.management;
 import com.easyjob.easyjobapi.modules.applierProfile.education.models.Education;
 import com.easyjob.easyjobapi.modules.applierProfile.education.models.EducationDAO;
 import com.easyjob.easyjobapi.modules.applierProfile.education.models.EducationId;
+import com.easyjob.easyjobapi.modules.applierProfile.education.models.EducationResponse;
 import com.easyjob.easyjobapi.modules.applierProfile.management.ApplierProfileMapper;
 import com.easyjob.easyjobapi.utils.CycleAvoidingMappingContext;
 import org.mapstruct.*;
@@ -16,6 +17,11 @@ public interface EducationMapper {
     @Mapping(target = "educationId", source = "toMap", qualifiedByName = "longToObject")
     Education mapToDomain(EducationDAO toMap,
                           @Context CycleAvoidingMappingContext cycleAvoidingMappingContext);
+
+    @Mapping(target = "educationId", source = "id")
+    @Mapping(target = "applierProfileId", source = "applierProfile.id")
+    EducationResponse mapToResponse(EducationDAO educationDAO,
+                                    @Context CycleAvoidingMappingContext cycleAvoidingMappingContext);
 
     @Named("longToObject")
     default EducationId fromLongToObject(EducationDAO educationDAO) {
--- src/main/java/com/easyjob/easyjobapi/modules/applierProfile/education/services/EducationGetService.java
+++ src/main/java/com/easyjob/easyjobapi/modules/applierProfile/education/services/EducationGetService.java
@@ -2,12 +2,14 @@ package com.easyjob.easyjobapi.modules.applierProfile.education.services;
 
 import com.easyjob.easyjobapi.core.user.models.UserDAO;
 import com.easyjob.easyjobapi.modules.applierProfile.education.management.EducationManager;
+import com.easyjob.easyjobapi.modules.applierProfile.education.management.EducationMapper;
 import com.easyjob.easyjobapi.modules.applierProfile.education.models.EducationDAO;
 import com.easyjob.easyjobapi.modules.applierProfile.education.models.EducationPageResponse;
 import com.easyjob.easyjobapi.modules.applierProfile.education.models.EducationResponse;
 import com.easyjob.easyjobapi.modules.applierProfile.management.ApplierProfileManager;
 import com.easyjob.easyjobapi.modules.applierProfile.management.ApplierProfileNotFoundException;
 import com.easyjob.easyjobapi.modules.applierProfile.models.ApplierProfileDAO;
+import com.easyjob.easyjobapi.utils.CycleAvoidingMappingContext;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +26,7 @@ import java.util.List;
 public class EducationGetService {
     private final HttpServletRequest request;
     private final EducationManager educationManager;
+    private final EducationMapper educationMapper;
     private final ApplierProfileManager applierProfileManager;
 
     public EducationPageResponse get(int limit, int page){
@@ -37,16 +40,7 @@ public class EducationGetService {
         Page<EducationDAO> educationDAOPage = educationManager.findByApplierProfile_Id(applierProfileDAO.getId(), pageable);
         List<EducationResponse> educationResponseList = educationDAOPage.get()
                 .filter(item -> item.getIsArchived() == false)
-                .map(item -> EducationResponse.builder()
-                        .educationId(item.getId())
-                        .applierProfileId(item.getApplierProfile().getId())
-                        .degree(item.getDegree())
-                        .university(item.getUniversity())
-                        .startDate(item.getStartDate())
-                        .endDate(item.getEndDate())
-                        .major(item.getMajor())
-                        .gpa(item.getGpa())
-                        .build())
+                .map(item -> educationMapper.mapToResponse(item, new CycleAvoidingMappingContext()))
                 .toList();
 
         return EducationPageResponse.builder()
