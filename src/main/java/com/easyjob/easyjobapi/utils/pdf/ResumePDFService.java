8f7254eea98c197d95dfb3f7dcad80011a3c4838
Roman Beznosenko <r.beznosenko@smartsoftsolutions.pl>
2025-12-28 12:35:27 +0100

    Deleted comments

--- src/main/java/com/easyjob/easyjobapi/utils/pdf/ResumePDFService.java
+++ src/main/java/com/easyjob/easyjobapi/utils/pdf/ResumePDFService.java
@@ -8,25 +8,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-/**
- * Main service for CV/Resume PDF generation
- * Uses Strategy Pattern to support multiple CV styles
- */
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ResumePDFService {
-
     private final CVStrategyFactory strategyFactory;
 
-    /**
-     * Generate PDF using specified template style
-     *
-     * @param cvData The CV data
-     * @param template The desired CV style (MODERN, CREATIVE, CORPORATE, MINIMAL)
-     * @return PDF bytes
-     * @throws Exception if generation fails
-     */
     public byte[] generatePDF(ApplierProfileCVResponse cvData, CVTemplateEnum template) throws Exception {
         log.info("Generating PDF with template: {}", template);
 
@@ -46,25 +33,5 @@ public class ResumePDFService {
                 template, pdfBytes.length);
 
         return pdfBytes;
-    }
-
-    /**
-     * Generate PDF using default (MODERN) template
-     *
-     * @param cvData The CV data
-     * @return PDF bytes
-     * @throws Exception if generation fails
-     */
-    public byte[] generatePDF(ApplierProfileCVResponse cvData) throws Exception {
-        return generatePDF(cvData, CVTemplateEnum.MODERN);
-    }
-
-    /**
-     * Get list of all available CV template styles
-     *
-     * @return List of available style names
-     */
-    public java.util.List<String> getAvailableTemplates() {
-        return strategyFactory.getAvailableStyles();
     }
 }
